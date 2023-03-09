cd6c5122d56edee62ce2a1204e463b3852877e37
Ernest <musaubrian15@gmail.com>
2023-03-09 10:35:04 +0300

    chore: setup project

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import {PrismaClient} from "@prisma/client"
+import express from "express"
+
+
+const app = express();
+
+app.use(express.json())
+
+app.listen(3000, ()=> console.log("Listening on port 3000"))

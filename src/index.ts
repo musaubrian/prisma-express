8f09d058400be7387cdd14589e4d149ed57e335a
Ernest <musaubrian15@gmail.com>
2023-03-09 10:51:07 +0300

    chore: add routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import {PrismaClient} from "@prisma/client"
 import express from "express"
 
-
+const prisma = new PrismaClient()
 const app = express();
 
 app.use(express.json())
 
-app.listen(3000, ()=> console.log("Listening on port 3000"))
+app.get("/users", async(req, res) => {
+    const users = await prisma.user.findMany()
+    res.json(users)
+})
+
+app.post("/users/new",async (req, res) => {
+    const {email, name } = req.body
+    const created_user = await prisma.user.create({
+        data: {
+            email,
+            name
+        }
+    })
+    res.json(created_user)
+})
+
+app.listen(3000, () => console.log("Listening on port 3000"))

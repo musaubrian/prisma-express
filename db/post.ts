5272cf7fc6463b6c8f699227d0cf2e6bdd065550
Ernest <musaubrian15@gmail.com>
2023-03-10 10:57:06 +0300

    chore: get a single post

--- db/post.ts
+++ db/post.ts
@@ -1,23 +1,29 @@
-import { PrismaClient } from "@prisma/client"
-import { findUserbyID } from "./user"
+import { PrismaClient } from "@prisma/client";
+import { findUserbyID } from "./user";
 
-const prisma = new PrismaClient
+const prisma = new PrismaClient();
 
-
-export async function newPost(userID: number, title: string, content: string){
-    const user = await findUserbyID(userID)
-
-    return await prisma.post.create({
-        data: {
-            title,
-            content,
-            author: {connect: {email: user?.email}}
-        }
-    })
+export async function allPosts() {
+  return await prisma.post.findMany({
+    include: { author: true },
+  });
 }
 
-export async function allPosts(){
-    return await prisma.post.findMany({
-        include: {author: true}
-    })
+export async function newPost(userID: number, title: string, content: string) {
+  const user = await findUserbyID(userID);
+
+  return await prisma.post.create({
+    data: {
+      title,
+      content,
+      author: { connect: { email: user?.email } },
+    },
+  });
+}
+
+export async function findPost(postID: number) {
+  return await prisma.post.findUnique({
+    where: { id: postID },
+    include: { author: true },
+  });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,47 @@
-import express from "express"
-import { allPosts, newPost } from "../db/post";
+import express from "express";
+import { allPosts, findPost, newPost } from "../db/post";
 import { findUserbyID, listUsers, newUser } from "../db/user";
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/users", async(req, res) => {
-    const users = await listUsers()
-    res.json(users)
-})
+app.get("/users", async (req, res) => {
+  const users = await listUsers();
+  res.json(users);
+});
 
-app.post("/users/new", async(req, res) => {
-    const {name, email} = req.body
-    const user = await newUser(name, email)
-    res.json(user)
-})
+app.post("/users/new", async (req, res) => {
+  const { name, email } = req.body;
+  const user = await newUser(name, email);
+  res.json(user);
+});
 
 app.get("/users/:id", async (req, res) => {
-    const {id} = req.params
-    const intID = parseInt(id)
-    const result = await findUserbyID(intID)
-    res.json(result)    
-})
+  const { id } = req.params;
+  const intID = parseInt(id);
+  const result = await findUserbyID(intID);
+  res.json(result);
+});
 
-app.get("/posts",async (req, res) => {
-    const result = await allPosts()
-    res.json(result)    
-})
+app.get("/posts", async (req, res) => {
+  const result = await allPosts();
+  res.json(result);
+});
 
-app.post("/users/:id/posts/new",async (req, res) => {
-    const {id} = req.params
-    const {title, content} = req.body
-    const userID = parseInt(id)
-    const result = await newPost(userID, title, content)
+app.get("/posts/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
 
-    res.json(result)
+  const result = await findPost(id);
+  res.json(result);
+});
 
-})
-app.listen(3000, () => console.log("Listening on port 3000"))
+app.post("/users/:id/posts/new", async (req, res) => {
+  const userID = parseInt(req.params.id);
+  const { title, content } = req.body;
+
+  const result = await newPost(userID, title, content);
+
+  res.json(result);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000"));
